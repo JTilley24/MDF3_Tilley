1fc9536b03dab2198076d1e5b89277495fb07822
JTilley24 <justin.tilley@me.com>
2014-02-20 17:16:54 -0500

    Added Widget and WidgetConfig

---
+++ Project 3/PicturePlaces W&A/src/com/jtilley/pictureplaces/PicWidgetProvider.java
@@ -0,0 +1,22 @@
+package com.jtilley.pictureplaces;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class PicWidgetProvider extends AppWidgetProvider{
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onDeleted(context, appWidgetIds);
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+}
