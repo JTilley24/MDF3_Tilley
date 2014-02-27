fa2ab739acc82754b66eb024203797773160fa85
JTilley24 <justin.tilley@me.com>
2014-02-26 20:54:43 -0500

    Added saving account and validate login

--- Project 4/SportProfile/assets/www/js/main.js
+++ Project 4/SportProfile/assets/www/js/main.js
@@ -1,5 +1,16 @@
 $("#home").on("pageinit", function(){
-	
+	$("#login").on("click", function(){
+		var email = $("#enterEmail").val();
+		var password = $("#enterPassword").val();
+		//webview.getLogin(email, password);
+		webview.hasEmail(email);
+		var check = webview.hasEmail(email);
+		webview.getBoolean(check);
+		if(check){
+			webview.hasPassword(email, password);
+		}
+		
+	});
 });
 
 $("#account").on("pageinit", function(){
@@ -8,27 +19,36 @@ $("#account").on("pageinit", function(){
 			$("#team").empty();
 			$("#team").selectmenu("refresh", true);
 		}else{
-			console.log($("#sport").val());
 			loadData($("#sport").val());
 			$("#team").val([]);
 		}
+	});
+	
+	$("#submit").on("click", function(){
+		
+		saveData();
+		
 	})
 
 });
 
-
+var jsonData;
 
 var loadData = function(sport){
 	
 	$.getJSON("data/data.json", function(data){
 		if(sport === "MLB"){
 			displayTeam(data.MLB);
+			jsonData = data.MLB;
 		}else if(sport === "NFL"){
 			displayTeam(data.NFL);
+			jsonData = data.NFL;
 		}else if(sport === "NHL"){
 			displayTeam(data.NHL);
+			jsonData = data.NHL;
 		}else if(sport === "NBA"){
 			displayTeam(data.NBA);
+			jsonData = data.NBA;
 		}
 		
 	})
@@ -40,9 +60,24 @@ var displayTeam = function(data){
 	
 	for(var i=0; i < data.length; i++){
 		var name = data[i].location + " " + data[i].name;
-		console.log(name);
+		
 		var sublist = $("<option value='"+ i +"'>"+ name + "</option>")
 		sublist.appendTo("#team");
 	}
 	$("#team").selectmenu("refresh", true);
 };
+
+var saveData = function(){
+	jsonObj = [];
+	item = {};
+	item["firstname"] = $("#firstname").val();
+	item["lastname"] = $("#lastname").val();
+	item["email"] = $("#email").val();
+	item["password"] = $("#password").val();
+	item["sport"] = $("#sport").val();
+	item["team"] = jsonData[$("#team").val()];
+	jsonObj.push(item);
+	console.log(jsonObj);
+	webview.getString(JSON.stringify(jsonObj));
+	webview.getAccount(JSON.stringify(jsonObj));
+}
--- Project 4/SportProfile/src/com/jtilley/sportprofile/MainActivity.java
+++ Project 4/SportProfile/src/com/jtilley/sportprofile/MainActivity.java
@@ -1,11 +1,20 @@
 package com.jtilley.sportprofile;
 
 
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
@@ -14,9 +23,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		SharedPreferences prefs = this.getSharedPreferences("user_profile", 0);
+		if(prefs.getAll().size() > 0){
+			Log.i("PREFS", prefs.getAll().toString());
+		}
+		
 		WebView webView = (WebView) findViewById(R.id.webView1);
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.addJavascriptInterface(new WebAppInterface(this), "native");
+		WebSettings settings = webView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setAllowFileAccessFromFileURLs(true);
+		settings.setAllowUniversalAccessFromFileURLs(true);
+		webView.addJavascriptInterface(new WebAppInterface(this), "webview");
 		webView.loadUrl("file:///android_asset/www/index.html");
 	}
 
@@ -35,9 +53,71 @@ public class MainActivity extends Activity {
 		}
 		
 		@JavascriptInterface
-		public String getString(String text){
-			String newText = text + "NEW";
-			return newText;
+		public void getString(String text){
+			Log.i("TEXT", text);
+		}
+		
+		@JavascriptInterface
+		public void getAccount(String account){
+			//Log.i("ACCOUNT", account);
+			try {
+				JSONArray jsonArray = new JSONArray(account);
+				JSONObject jsonObj = jsonArray.getJSONObject(0);
+				//Log.i("ACCOUNT_NAME", jsonObj.getString("firstname"));
+				SharedPreferences prefs = mContext.getSharedPreferences("user_profile", 0);
+				SharedPreferences.Editor editPrefs = prefs.edit();
+				editPrefs.putString(jsonObj.getString("email"), jsonObj.toString());
+				editPrefs.commit();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public Boolean hasEmail(String email){
+			SharedPreferences prefs = mContext.getSharedPreferences("user_profile", 0);
+			Map<String, ?> accounts = prefs.getAll();
+			for(int i=0; i<accounts.size();i++){
+				if(accounts.containsKey(email)){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		public void getBoolean(Boolean check){
+			Log.i("BOOLEAN", check.toString());
+		}
+		
+		public Boolean hasPassword(String email, String password){
+			SharedPreferences prefs = mContext.getSharedPreferences("user_profile", 0);
+			String account = prefs.getString(email, null);
+			Log.i("EMAIL", "account");
+			if(account != null){
+				try {
+					JSONObject jsonObject = new JSONObject(account);
+					if(jsonObject.getString("password").equalsIgnoreCase(password)){
+						Log.i("PASSWORD", jsonObject.getString("password"));
+						return true;
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			return false;
+		}
+		
+		public void getLogin(String email, String password){
+			SharedPreferences prefs = mContext.getSharedPreferences("user_profile", 0);
+			Map<String, ?> accounts = prefs.getAll();
+			for(int i=0; i<accounts.size();i++){
+				if(accounts.containsKey(email)){
+					Log.i("ACCOUNT", accounts.get(email).toString());
+				}
+			}
+			//Log.i("LOGIN", email + " " + password);
 		}
 	}
 	
